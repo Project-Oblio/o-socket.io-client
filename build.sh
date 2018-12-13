5e2910d0763b2168b1253743a2d32816ce8b2352
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2018-12-12 23:32:07 -0500

    update thing

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 browserify socketClient.js --standalone SocketClient > socketClient.min.js;
-cp socketClient.min.js ~/music/laravel/resources/assets/js/libs/;
+cp socketClient.min.js ../music/laravel/resources/assets/js/libs/;
