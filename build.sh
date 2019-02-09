d50dde3e14aae637fc3fb14782965c7192c4c911
Satoshi Quasimodo <38660876+projectoblio@users.noreply.github.com>
2019-02-09 18:35:32 -0500

    stash

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-browserify socketClient.js --standalone SocketClient > socketClient.min.js;
-cp socketClient.min.js ../music/laravel/resources/assets/js/libs/;
-cp socketClient.min.js ../music/laravel/resources/assets/util/lib/;
+browserify socketClient.js --standalone SocketClient > socketClient.raw.js;
+minify socketClient.raw.js -d tempdist;
+mv ./tempdist/socketClient.raw.js ./dist/socketClient.min.js;
+rm -rf tempdist
+mv socketClient.raw.js ./dist;
+cp ./dist/socketClient.min.js ../music/laravel/resources/assets/js/libs/;
+cp ./dist/socketClient.min.js ../music/laravel/resources/assets/util/lib/;
 musicYarn
